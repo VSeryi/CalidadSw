de70fbdf460d7a752d7c6109cc674aa50da8250e
César Valdés <cesaramvm@gmail.com>
2016-02-27 16:41:24 +0100

    Principio de Pruebas

---
+++ comunio/app/src/androidTest/java/com/comunio/grupo8/comunio/PruebaTest.java
@@ -0,0 +1,14 @@
+package com.comunio.grupo8.comunio;
+
+/**
+ * Created by sobremesa on 27/02/16.
+ */
+
+
+        import org.junit.runner.RunWith;
+        import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class PruebaTest {
+}
+
