afe633f1f129c805aa506b03c09cbbaac0312193
BuildTools <unconfigured@null.spigotmc.org>
2016-02-28 15:08:07 +0100

    Generado los test .java desde los .features restantes

---
+++ comunio/app/src/androidTest/java/com/comunio/grupo8/comunio/ClasificacionTest.java
@@ -0,0 +1,59 @@
+package my.package.name
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class MyStepDefinitions {
+
+    @Given("^There are users in the application$")
+    public void there_are_users_in_the_application() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^the user presses the Leaderboard button$")
+    public void the_user_presses_the_leaderboard_button() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^an user wins a match$")
+    public void an_user_wins_a_match() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the user should view the leaderboard$")
+    public void the_user_should_view_the_leaderboard() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the match points should be added to the user's score$")
+    public void the_match_points_should_be_added_to_the_users_score() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the user's position in the leaderboard should change according to his score$")
+    public void the_users_position_in_the_leaderboard_should_change_according_to_his_score() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^all the users should appear with their points$")
+    public void all_the_users_should_appear_with_their_points() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^it should be reflected in the Leaderboard$")
+    public void it_should_be_reflected_in_the_leaderboard() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^his points are grater than other user$")
+    public void his_points_are_grater_than_other_user() throws Throwable {
+        throw new PendingException();
+    }
+
+}
---
+++ comunio/app/src/androidTest/java/com/comunio/grupo8/comunio/MercadoTest.java
@@ -0,0 +1,69 @@
+package my.package.name
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class MyStepDefinitions {
+
+    @Given("^there is at least 1 Player in the market$")
+    public void there_is_at_least_1_player_in_the_market() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^the team at least has 1 Player$")
+    public void the_team_at_least_has_1_player() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^the market is empty$")
+    public void the_market_is_empty() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^the user presses the Buy button$")
+    public void the_user_presses_the_buy_button() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^the user presses the Sell button$")
+    public void the_user_presses_the_sell_button() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^the user acceses the market$")
+    public void the_user_acceses_the_market() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the Player should be added to the user's team$")
+    public void the_player_should_be_added_to_the_users_team() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the Player should be removed from the user's team$")
+    public void the_player_should_be_removed_from_the_users_team() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^it should display a message saying the market is empty$")
+    public void it_should_display_a_message_saying_the_market_is_empty() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^the Player should be removed from the market$")
+    public void the_player_should_be_removed_from_the_market() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^the Player should be added to the market$")
+    public void the_player_should_be_added_to_the_market() throws Throwable {
+        throw new PendingException();
+    }
+
+}
